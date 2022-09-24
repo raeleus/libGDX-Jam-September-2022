cd50950a7539b81f62bb3c2dbad6bc250df94c73
raeleus <raymond.ray3k@gmail.com>
2022-09-23 19:17:55 -0700

    Added ListUpdaterLite!

---
+++ listupdater/src/main/java/com/ray3k/template/listupdater/ListUpdaterLite.java
@@ -0,0 +1,24 @@
+package com.ray3k.template.listupdater;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ListUpdaterLite {
+    private static String levelFolderPath = "C:/onedrive/workspace/Jam Games/libGDX Jam September 2022/design/output/levels";
+    
+    public static void main(String args[]) throws IOException {
+        if (levelFolderPath != null) {
+            var sourcePath = Paths.get(levelFolderPath);
+            Files.walk(sourcePath).forEach(source -> {
+                        Path destination = Paths.get("assets/levels/", source.toString().substring(levelFolderPath.length()));
+                        try {
+                            Files.copy(source, destination);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
+    }
+}
